c77d04ce4c55eb98b95d78c9d699fa30fa5b91dd
xooseph <josephlsalgado@outlook.com>
2025-08-16 02:28:23 -0600

    feat: add and draw friends

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = [];
+
+function addFriend() {
+    let friendInput = document.getElementById('friend').value;
+
+    if (friendInput === '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        friends.push(friendInput);
+        document.getElementById('friend').value = '';
+
+        updateFriendList();
+    }
+
+    return;
+}
+
+function updateFriendList() {
+    let friendList = document.getElementById('friendList');
+    friendList.innerHTML = '';
+
+    for (let i = 0; i < friends.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = friends[i];
+        friendList.appendChild(li);
+    }
+
+    return;
+}
+
+function drawFriend() {
+    if (friends.length < 2) {
+        alert('Necesita al menos dos amigos para realizar el sorteo del Amigo Secreto.');
+        return;
+    }
+
+    let index = Math.floor(Math.random() * friends.length);
+
+    let selectedFriend = friends[index];
+
+    let resultFriend = document.getElementById('result');
+    
+    resultFriend.innerHTML = selectedFriend;
+}
